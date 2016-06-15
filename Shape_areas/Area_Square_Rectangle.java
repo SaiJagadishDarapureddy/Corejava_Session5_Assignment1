0cb62db1ad2b5eb8495c75ec711a24fb066790e2
SaiJagadishDarapureddy <sai.jagadish.d@gmail.com>
2016-06-15 00:12:02 +0000

    Add files via upload

---
+++ Shape_areas/Area_Square_Rectangle.java
@@ -0,0 +1,15 @@
+package Shape_areas;
+
+public class Area_Square_Rectangle{
+	int Side=40;
+	int Width=80;
+	
+void Area_Squ(){
+		
+		System.out.println("AREA OF THE SQUARE IS "+  Side*Side +" SqCm");
+	}
+void Area_Rect(){
+	
+	System.out.println("AREA OF THE RECTANGLE IS " +(Side*Width)+" SqCm" );
+}
+}
---
+++ Shape_areas/Inheritance_Test.java
@@ -0,0 +1,13 @@
+package Shape_areas;
+
+public class Inheritance_Test extends Area_Square_Rectangle{
+
+	public static void main(String[] args) {
+		
+		Inheritance_Test Area= new Inheritance_Test() ;
+		System.out.println("*************AREAS**************");
+		
+	Area.Area_Squ();
+	Area.Area_Rect();
+	}
+}
